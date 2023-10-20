03589d6e4b870fed04c6ef1dc7a475c3cfa646ab
Eulises Franco <francoeulises@yahoo.com.co>
2023-10-20 17:44:20 -0500

    Latest Working

--- src/components/Common/SiteFooter.js
+++ src/components/Common/SiteFooter.js
@@ -1,15 +1,49 @@
 import Container from 'react-bootstrap/Container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // You'll need to install and setup FontAwesome with React to use this.
 
 function SiteFooter() {
-    return (<
-        footer className='py-5 my-5'>
-            <Container className='px-4'>
-                <p className='text-end'>&copy; 2023</p>
-            </Container>
+    return (
+        <footer className='footer'>
+        
+            <Container className='py-5'>
+                <div className="box-container">
+                    
+                    <div className="box">
+                        <h3>quick links</h3>
+                        {["home", "shop", "about", "review", "contact"].map(link => (
+                            <a href={`${link}.html`} key={link}>
+                                <FontAwesomeIcon icon="arrow-right" /> {link}
+                            </a>
+                        ))}
+                    </div>
 
-    
+                    <div className="box">
+                        <h3>extra links</h3>
+                        {["my order", "my favorite", "my wishlist", "my account", "terms of use"].map(link => (
+                            <a href="#" key={link}>
+                                <FontAwesomeIcon icon="arrow-right" /> {link}
+                            </a>
+                        ))}
+                    </div>
+
+                    <div className="box">
+                        <h3>newsletter</h3>
+                        <p>subscribe for latest updates</p>
+                        <form action="">
+                            <input type="email" placeholder="enter your email" />
+                            <input type="submit" value="subscribe" className="btn" />
+                        </form>
+                        <img src="image/payment.png" className="payment" alt="" />
+                    </div>
+
+                </div>
+
+                <div className="credit text-center mt-5">
+                    created by Pantry Parcel Team | all rights reserved!
+                </div>
+            </Container>
         </footer>
-    )
+    );
 }
 
 export default SiteFooter;
--- src/components/home/HomePage1.js
+++ src/components/home/HomePage1.js
@@ -1,79 +1,92 @@
-import Container from "react-bootstrap/Container"
+import React, { useEffect, useState } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import Container from "react-bootstrap/Container";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Button from "react-bootstrap/Button";
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
-import React, { useEffect, useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel'; // <-- Import Carousel for slider
 import { createTodo } from '../../graphql/mutations';
 import { listTodos } from '../../graphql/queries';
-import { API, graphqlOperation } from 'aws-amplify';
+import homeBg from '../image/home-bg.jpg';
+import home_img_1 from '../image/home-img-1.png';
+import home_img_2 from '../image/home-img-2.png';
+import home_img_3 from '../image/home-img-3.png';
+import banner1 from '../image/banner-1.jpg';
+import banner2 from '../image/banner-2.jpg';
+import banner3 from '../image/banner-3.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './HomePage1.css'
 
 
 function HomePage() {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        fetchProduct()
+        fetchProduct();
     }, []);
 
     const fetchProduct = async () => {
-        try{
-        const productData = await API.graphql(graphqlOperation( listTodos ));
-        const productList = productData.data.listTodos.items;
-        console.log('Produts list', productList);
-        setProducts(productList);
+        try {
+            const productData = await API.graphql(graphqlOperation(listTodos));
+            const productList = productData.data.listTodos.items;
+            setProducts(productList);
         } catch (error) {
-        console.log('error on fetching products', error);
+            console.log('error on fetching products', error);
         }
     }
-    return (
-        <Container fluid>
-            {/* Products Display */}
-            <Row className="px-4 my-5">
-                {products.map((product, index) => (
-                    <Col xs={12} sm={6} md={4} lg={3} key={product.productID || index}>
-                        <Card>
-                            <Card.Img variant="top" src={product.productImage || "/img/sample-product.jpg"} />
-                            <Card.Body>
-                                <Card.Title>{product.productName || `Product ${index + 1}`}</Card.Title>
-                                <Card.Text>
-                                    {product.productDescription || "Some brief product description."}
-                                </Card.Text>
-                                <Button variant="primary">View Product</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
 
-            {/* Tabs Section */}
-            <Row className="px-4 my-5">
+    return (
+        <Container fluid style={{ 
+            backgroundImage: `url(${homeBg})`, 
+            backgroundSize: 'cover', 
+            backgroundPosition: 'center center',
+            backgroundRepeat: 'no-repeat'
+        }}>
+            {/* Slider Component */}
+            <Row className="my-5">
                 <Col xs={12}>
-                    <Tabs defaultActiveKey="home" id="product-tabs">
-                        <Tab eventKey="home" title="Home">
-                            {/* You can replace this with your products component */}
-                            <p>Home Products</p>
-                        </Tab>
-                        <Tab eventKey="electronics" title="Electronics">
-                            {/* You can replace this with your products component */}
-                            <p>Electronics Products</p>
-                        </Tab>
-                        <Tab eventKey="clothing" title="Clothing">
-                            {/* You can replace this with your products component */}
-                            <p>Clothing Products</p>
-                        </Tab>
-                        <Tab eventKey="accessories" title="Accessories">
-                            {/* You can replace this with your products component */}
-                            <p>Accessories Products</p>
-                        </Tab>
-                        {/* Add more tabs as needed */}
-                    </Tabs>
+                    <Carousel>
+                        {[home_img_1, home_img_2, home_img_3].map((imgSrc, index) => (
+                            <Carousel.Item key={index}>
+                            <Row className="carousel-content">
+                                <Col md={6} className="content-container">
+                                    <div className="content">
+                                        <span>fresh and organic</span>
+                                        <h3>upto 50% off</h3>
+                                        <Button variant="primary">Shop Now</Button>
+                                    </div>
+                                </Col>
+                                <Col md={6}>
+                                    <img
+                                        className="carousel-img"
+                                        src={imgSrc}
+                                        alt={`Slide ${index + 1}`}
+                                    />
+                                </Col>
+                            </Row>
+                        </Carousel.Item>
+                        ))}
+                    </Carousel>
                 </Col>
             </Row>
+                        
+            <section className="banner-container">
+                {[banner1, banner2, banner3].map((bannerImg, index) => (
+                    <div className="banner" key={index}>
+                        <img src={bannerImg} alt={`Banner ${index + 1}`} />
+                        <div className="content">
+                            <span>limited sales</span>
+                            <h3>upto 50% off</h3>
+                            <a href="#" className="btn">shop now</a>
+                        </div>
+                    </div>
+                ))}
+            </section>
+
         </Container>
     )
 }
-
 export default HomePage;

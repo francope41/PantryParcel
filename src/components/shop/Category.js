838b59f734f6f09843e77316ed857ccc6a4f5183
Eulises <francoeulises@yahoo.com.co>
2023-11-06 16:44:20 -0600

    Latest Updates: Added category images, modified shop.css

--- src/components/shop/Category.js
+++ src/components/shop/Category.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import cat1 from '../image/cat-1.png';
-import cat2 from '../image/cat-2.png';
-import cat3 from '../image/cat-3.png';
-import cat4 from '../image/cat-4.png';
-import cat5 from '../image/cat-5.png';
+import cat1 from '../image/fruits.png';
+import cat2 from '../image/vegetables.png';
+import cat3 from '../image/Chips.png';
+import cat4 from '../image/juice.png';
+import cat5 from '../image/BreakfastSpecial.png';
+import cat6 from '../image/chocolate.png';
+import cat7 from '../image/cookies.png';
+import cat8 from '../image/dairy products.png';
+
 function Category({ onCategoryClick }) {
   const categories = [
     { id: 1, image: cat1, name: "Fresh Fruits" },
@@ -11,9 +15,9 @@ function Category({ onCategoryClick }) {
     { id: 3, image: cat3, name: "Chips" },
     { id: 4, image: cat4, name: "Juice" },
     { id: 5, image: cat5, name: "Breakfast Specials" },
-    { id: 6, image: cat5, name: "Chocolates" },
-    { id: 7, image: cat5, name: "Cookies" },
-    { id: 8, image: cat5, name: "Dairy products" },
+    { id: 6, image: cat6, name: "Chocolates" },
+    { id: 7, image: cat7, name: "Cookies" },
+    { id: 8, image: cat8, name: "Dairy products" },
   ];
 
   return (
@@ -22,7 +26,7 @@ function Category({ onCategoryClick }) {
       <div className="box-container">
         {categories.map(category => (
           <a href="#" className="box" key={category.id} onClick={() => onCategoryClick(category.name)}>
-            <img src={category.image} alt="" />
+            <img src={category.image} alt={category.name} className="category-image" />
             <h3>{category.name}</h3>
           </a>
         ))}
